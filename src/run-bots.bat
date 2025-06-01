9aa069ff2e9a70bcf36b4fb89007beb1a33cf46f
Daaccd <brahmantio05@gmail.com>
2025-06-01 21:27:58 +0700

    Update

--- src/run-bots.bat
+++ src/run-bots.bat
@@ -1,5 +1,5 @@
 @echo off
 start cmd /c "python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo"
-start cmd /c "python main.py --logic MyBot--email=test1@email.com --name=test --password=123456 --team etimo"
+start cmd /c "python main.py --logic Pesemka--email=test1@email.com --name=test --password=123456 --team etimo"
 start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo"
-start cmd /c "python main.py --logic MyBot --email=test3@email.com --name=test --password=123456 --team etimo"
+start cmd /c "python main.py --logic Pesemka --email=test3@email.com --name=test --password=123456 --team etimo"
